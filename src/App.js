8329f3757f59ea8e70f37089a2cc1fe428c53ced
Pratap Tyagi <tyagipratap111@gmail.com>
2021-04-14 23:16:09 +0530

    Basic form functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,105 @@
-import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [userRegistration, setUserRegistration] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const [records, setRecords] = useState([]);
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserRegistration({ ...userRegistration, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newRecords = {
+      ...userRegistration,
+      id: new Date().getTime().toString(),
+    };
+
+    setRecords([...records, newRecords]);
+    setUserRegistration({
+      username: "",
+      email: "",
+      phone: 0,
+      password: "",
+    });
+  };
+
   return (
-    <div className="App">
-      <h1>React Form</h1>
-    </div>
+    <>
+      <form action="" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Full Name</label>
+          <input
+            type="text"
+            autoComplete="off"
+            value={userRegistration.username}
+            name="username"
+            onChange={handleChange}
+            id="username"
+            placeholder="Enter Full-Name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            autoComplete="off"
+            value={userRegistration.email}
+            name="email"
+            onChange={handleChange}
+            id="email"
+            placeholder="Enter Email"
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone number</label>
+          <input
+            type="text"
+            autoComplete="off"
+            value={userRegistration.phone}
+            name="phone"
+            onChange={handleChange}
+            id="phone"
+            placeholder="Enter Phone number"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            autoComplete="off"
+            value={userRegistration.password}
+            name="password"
+            onChange={handleChange}
+            id="password"
+            placeholder="Enter password"
+          />
+        </div>
+        <button type="submit">Registration</button>
+      </form>
+      <div>
+        {records.map((record) => {
+          const { id, username, email, password, phone } = record;
+          return (
+            <div className="showDataStyles" key={id}>
+              <p>{username}</p>
+              <p>{email}</p>
+              <p>{phone}</p>
+              <p>{password}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
